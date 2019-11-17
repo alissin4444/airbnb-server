c8e7e9a8a16a925fe3356789ed1c8e5654b58aa4
Alisson Santos <45664013+alissin4444@users.noreply.github.com>
2019-11-17 15:56:46 -0300

    Add files via upload

---
+++ env.js
@@ -0,0 +1,14 @@
+HOST=127.0.0.1
+PORT=3333
+NODE_ENV=development
+APP_NAME=AdonisJs
+APP_URL=http://${HOST}:${PORT}
+CACHE_VIEWS=false
+APP_KEY=GpBeabkdDm6Vr8oj4KusXetQDGn4TE4s
+DB_CONNECTION=pg
+DB_HOST=isilo.db.elephantsql.com
+DB_PORT=5432
+DB_USER=sfiyburz
+DB_PASSWORD=3eOfpMue4VOKwIfmWMuWieAY0br4C1Y5
+DB_DATABASE=sfiyburz
+HASH_DRIVER=bcrypt
